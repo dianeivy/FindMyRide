681f50bf9878c60690940f119b94e28c9517f5dc
Diane Ivy <dianeivy@gmail.com>
2016-08-03 19:53:41 -0700

    updated addresses

--- interface_scripts/find_starting_address.py
+++ interface_scripts/find_starting_address.py
@@ -1,9 +1,9 @@
 import googlemaps
 from datetime import datetime
 
-gmaps = googlemaps.Client(key='AIzaSyDHyED6bTDCiE5ixR3hzkeyONB132AO64s')
+gmaps = googlemaps.Client(key=tmp_key)
 
-geocode_result = gmaps.geocode('227 Charles Street, Cambridge, MA')
+geocode_result = gmaps.geocode('Cambridge, MA')
 
 
 starting_lat = geocode_result[0]['geometry']['location']['lat']
